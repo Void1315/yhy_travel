2453fc5a968151f8b92449dff3ac9383c706d58b
asahi <381848900@qq.com>
2019-10-27 22:19:54 +0800

    去掉path库

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -8,8 +8,6 @@
  * @LastEditTime: 2019-10-20 17:37:57
  * @Description: 使用单例模式的配置文件，
  */
-import 'package:path_provider/path_provider.dart';
-
 class Env {
   static String DIR_PATH;// 项目路径
   static bool IS_DEVELOPMENT = true; // 是否为开发环境
@@ -18,7 +16,6 @@ class Env {
   static Env _instance;// 单例
   Env._internal() {
     // 初始化
-    this._getDirPath();
   }
 
   static Env _getInstance() {
@@ -26,9 +23,5 @@ class Env {
       _instance = new Env._internal();
     }
     return _instance;
-  }
-
-  _getDirPath() async {
-    DIR_PATH = (await getApplicationDocumentsDirectory()).path;
   }
 }
