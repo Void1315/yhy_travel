ed8e61114ecdf9fbf3fb9b687464795dbebb7826
asahi <381848900@qq.com>
2019-11-09 23:23:43 +0800

    :sparkles: 引入图标库

--- lib/config/routers.dart
+++ lib/config/routers.dart
@@ -4,11 +4,12 @@
  * @Company: didi
  * @Date: 2019-10-26 23:46:42
  * @LastEditors: 脑瘫码农
- * @LastEditTime: 2019-11-09 14:18:23
+ * @LastEditTime: 2019-11-09 14:43:03
  * @Description: 路由文件
  */
 import 'package:flutter/material.dart';
-import '../pages/login/index.dart';
+import '../pages/login/exports.dart';
 Map<String, WidgetBuilder> router = {
   "/":(BuildContext context)=>LoginIndex(),
+  "/login":(BuildContext context)=>LoginIndex(),
 };
---
+++ lib/pages/login/login.dart
@@ -0,0 +1,25 @@
+/*
+ * @Author: 脑瘫码农
+ * @Email: yuhaoyan_i@didiglobal.com
+ * @Company: didi
+ * @Date: 2019-10-31 11:55:36
+ * @LastEditors: 脑瘫码农
+ * @LastEditTime: 2019-11-09 14:46:31
+ * @Description: 登录页面，有状态组件
+ */
+import 'package:flutter/cupertino.dart';
+
+class LoginPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+  
+}
+class _State extends State<LoginPage>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
